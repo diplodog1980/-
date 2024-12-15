b39b7b9be258012304dd3272ba38af1a05b81676
diplodog1980 <daniilmarakulin@yandex.ru>
2024-12-15 20:51:42 +0300

    edit 3.1

--- Лабораторная работа №3.1/app.js
+++ Лабораторная работа №3.1/app.js
@@ -1,20 +1,20 @@
 //задание1
-/*let user = { 
+let user1 = { 
     name: "Джон",
     age: 30
   };
-  Добавим метод для объекта
-  user.sayHi = function() {
+  //Добавим метод для объекта
+  user1.sayHi = function() {
     alert("Привет!");
   };
   
-  user.sayHi(); // Привет! 
+  user1.sayHi(); // Привет! 
 /*  Анализ: Здесь создаётся объект user с двумя свойствами: name и age.
 Далее добавляется метод sayHi, который выводит приветственное сообщение.
 Вызов user.sayHi() приводит к отображению окна с текстом "Привет!" */
 
 //задание2
-/* let customer = {
+ let customer = {
     name: "Алексей",
     age: 28,
     email: "alexey@gmail.com",
@@ -24,10 +24,10 @@
   
   console.log(customer.name);  // Алексей
   console.log(customer.email); // alexey@example.com
-  console.log(customer.phone);  // 7 800 555 3535 */
+  console.log(customer.phone);  // 7 800 555 3535 
   
   // адание 3
-  /* let user = {
+   let users = {
     name: "Светлана",
     age: 25
   };
@@ -45,26 +45,26 @@
   };
   
   // Условие для вывода информации
-  if (user.age > 21) {
-    console.log(`${user.name} сделал(а) заказ на сумму ${order.total} в ресторане ${restaurant.name}, коорый находится в "${restaurant.location}".`);
+  if (user1.age > 21) {
+    console.log(`${users.name} сделал(а) заказ на сумму ${order.total} в ресторане ${restaurant.name}, коорый находится в "${restaurant.location}".`);
   }
-  */
+
  
   // задание 4
-  /*
+
   function User(name) {
     this.name = name;
     this.isAdmin = false;
   }
   
-  let user = new User("Вася");
+  let user2 = new User("Вася");
   
-  console.log(user.name); // Вася
-  console.log(user.isAdmin); // false
-  */
+  console.log(user2.name); // Вася
+  console.log(user2.isAdmin); // false
+  
 
   //задание 5
-  /* function Student(name) {
+   function Student(name) {
   this.name = name;
 
   this.sayHi = function() {
@@ -75,7 +75,7 @@
 let vasya = new Student("Вася");
 
 vasya.sayHi(); // Меня зовут: Вася 
-*/
+
 
 //ДОП задние
 const calculator = {
@@ -108,5 +108,3 @@ console.log(calculator.subtract(5, 3));   // 2
 console.log(calculator.multiply(5, 3));   // 15
 console.log(calculator.divide(5, 3));     // 1.666...
 console.log(calculator.divide(5, 0));     // Ошибка: деление на ноль
-
-  
